1e0e0c52aa0027e683bb826625547ee596d579fc
joeldeans <joeldeans@gmail.com>
2019-08-01 15:58:27 -0600

    adjustmenta to styles

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,13 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-import NavLinks from "./nav-links.js";
 
 const Navbar = () => {
   return (
     <div className="navbar">
-      <div className="logo" />
-      <button className="btn-cart">shopping cart</button>
+      <div className="logo">
+        <img src="/assets/logo/your_car_needs_a_donut.jpg" alt="LOGO" />
+      </div>
+      <div className="btn">
+        <button className="cart">shopping cart</button>
+      </div>
     </div>
   );
 };
