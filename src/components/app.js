533c2c4767d6b68401fa507df6f8c52a5d445377
Emersonsilva <pexaos24@gmail.com>
2019-08-01 11:29:26 -0600

    picts and content

--- src/components/app.js
+++ src/components/app.js
@@ -8,14 +8,34 @@ export default class App extends Component {
       <div className="app">
         <div className="navbar">
           <div className="logo">
-            <img src="/assets/logo/donut_logo.png" />
+            <img src="/assets/logo/your_car_needs_a_donut.jpg" />
+            <div className="navbar-content">
+              <p>
+                You get a flat, and under the carpet in the trunk is one of
+                those "donut" (or "temporary") spare tires – skinnier than the
+                tires on your car, with a prominent warning about maximum speed
+                on the side. You're probably wondering how long you can drive on
+                it before you get your other tire repaired at a reputable shop.
+                We're here to help. The best place to start is your owner's
+                manual, which should list the exact recommended mileage for the
+                spare tire. If you don't have a manual, 70 miles is the rule of
+                thumb. These tires are not designed for long-range service. They
+                are made to get you to a garage to have your tire either
+                repaired or replaced. Driving on it for an extended period of
+                time under regular driving conditions will result in a blowout
+                in short order, so get a safe tire on your car immediately.
+                Maybe you can find a reasonably priced used tire if it has to be
+                replaced.
+              </p>
+            </div>
           </div>
+
           <button>shopping cart</button>
         </div>
 
         <RenderDonuts />
 
-        <div className="main-wapper"></div>
+        <div className="main-wapper" />
       </div>
     );
   }
