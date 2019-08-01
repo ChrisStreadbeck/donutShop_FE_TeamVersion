cda36af2f2f3a560a677dd4afb9fd7c7732d69ff
ChrisStreadbeck <han0987@gmail.com>
2019-08-01 11:13:33 -0600

    routing options and pages

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import RenderDonuts from "../actions/render-donuts";
+import Navigation from "../pages/navigation";
+import Login from "../pages/login";
 
 export default class App extends Component {
   render() {
@@ -10,6 +13,16 @@ export default class App extends Component {
           <div className="logo">
             <img src="/assets/logo/donut_logo.png" />
           </div>
+          <Router>
+            <div>
+              <Navigation />
+              <Switch>
+                <Route exact path="/" component={App} />
+                <Route path="/login" component={Login} />
+              </Switch>
+            </div>
+          </Router>
+
           <button>shopping cart</button>
         </div>
 
