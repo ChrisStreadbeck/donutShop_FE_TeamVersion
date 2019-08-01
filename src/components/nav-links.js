753f79ad0f6af68b4d5818583e83153b821d8134
ChrisStreadbeck <han0987@gmail.com>
2019-08-01 14:10:52 -0600

    refactored links

--- src/components/nav-links.js
+++ src/components/nav-links.js
@@ -1,19 +1,19 @@
-import React from "react";
-import { withRouter } from "react-router";
-import { NavLink } from "react-router-dom";
+// import React from "react";
+// import { withRouter } from "react-router";
+// import { NavLink } from "react-router-dom";
 
-const NavLinks = () => {
-  return (
-    <div className="nav-links">
-      <NavLink exact to="/" activeClassName="nav-link-active">
-        HOME
-      </NavLink>
+// const NavLinks = () => {
+//   return (
+//     <div className="nav-links">
+//       <NavLink exact to="/" activeClassName="nav-link-active">
+//         HOME
+//       </NavLink>
 
-      <NavLink exact to="/login" activeClassName="nav-link-active">
-        LOGIN
-      </NavLink>
-    </div>
-  );
-};
+//       <NavLink exact to="/login" activeClassName="nav-link-active">
+//         LOGIN
+//       </NavLink>
+//     </div>
+//   );
+// };
 
-export default withRouter(NavLinks);
+// export default withRouter(NavLinks);
