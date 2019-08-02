a9985bb21750e6702c4b525d0510ccd36e1e6ae8
ChrisStreadbeck <51247178+ChrisStreadbeck@users.noreply.github.com>
2019-08-02 17:57:56 -0600

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,12 +8,16 @@ const Navbar = () => {
         <img src="/assets/logo/your_car_needs_a_donut.jpg" alt="LOGO" />
       </div>
       <div div className="links">
-        <A href="/">Home</A>
-        <A href="/form">Form</A>
+        <div classID="home">
+          <A href="/">Home</A>
+        </div>
+        <div classID="form">
+          <A href="/form">Form</A>
+        </div>
       </div>
 
-      <div className="cart">
-        <button className="btn">shopping cart</button>
+      <div className="btn">
+        <button className="cart">shopping cart</button>
       </div>
     </div>
   );
