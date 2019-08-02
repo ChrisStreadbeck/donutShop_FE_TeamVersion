48620c2952e959c7fdf5100f4ecb6521f241ad0d
ChrisStreadbeck <han0987@gmail.com>
2019-08-02 15:01:36 -0600

    navbar fixes, added delete and edit buttons

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,24 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { useRoutes, A } from "hookrouter";
 
+import Navbar from "./components/navbar";
 import App from "./components/app";
 import DonutForm from "./pages/donut-form";
 
 import "./style/main.scss";
 
 const routes = {
-  "/": () => <App />,
-  "/form": () => <DonutForm />
+  "/": () => <App form={false} />,
+  "/form": () => <DonutForm form={true} />
 };
 
 function Main() {
   return (
-    <div>
-      <div className="navbar">
-        <A href="/">Home</A>
-        <A href="/form">Form</A>
-      </div>
+    <div className="body">
+      <Navbar />
       {useRoutes(routes)}
+      <App />
     </div>
   );
 }
