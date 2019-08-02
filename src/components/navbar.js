e5b8f53c872b066cc37d105cab469a3594564070
joeldeans <joeldeans@gmail.com>
2019-08-02 15:52:47 -0600

    Nav Styles

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,17 +4,20 @@ import { A } from "hookrouter";
 const Navbar = () => {
   return (
     <div className="nav-wrapper">
-      <div className="left-side">
-        <div className="logo">
-          <img src="/assets/logo/your_car_needs_a_donut.jpg" alt="LOGO" />
-        </div>
-        <A href="/">Home</A>
-        <A href="/form">Form</A>
+      <div className="logo">
+        <img src="/assets/logo/your_car_needs_a_donut.jpg" alt="LOGO" />
       </div>
-      <div className="right-side">
-        <div className="btn">
-          <button className="cart">shopping cart</button>
+      <div div className="links">
+        <div classID="home">
+          <A href="/">Home</A>
         </div>
+        <div classID="form">
+          <A href="/form">Form</A>
+        </div>
+      </div>
+
+      <div className="btn">
+        <button className="cart">shopping cart</button>
       </div>
     </div>
   );
